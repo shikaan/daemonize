adf2f8396f00b88cb04f09026a0d66f6e18c3f55
shikaan <spagnolo.manu@gmail.com>
2019-11-30 14:20:54 +0100

    Initial commit

---
+++ debounce.js
@@ -0,0 +1,12 @@
+
+exports.debounce = (func, sleep) => {
+    let timeoutId;
+  
+    return (...args) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+  
+      timeoutId = setTimeout(func, sleep, ...args);
+    }
+  }
